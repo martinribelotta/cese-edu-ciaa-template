29dc200b5c4e24167b4550ad07c820397596b21f
Martin Ribelotta <martinribelotta@gmail.com>
2018-11-06 19:16:30 -0300

    Some minor changes for cmdline
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- app/src/app.c
+++ app/src/app.c
@@ -1,77 +1,94 @@
-/*============================================================================
- * Autor:
- * Licencia:
- * Fecha:
- *===========================================================================*/
+#include <sapi.h>
+#include <stdio.h>
+#include <ctype.h>
 
-// Inlcusiones
+static char buffer[128];
 
-#include "app.h"         // <= Su propia cabecera
-#include "sapi.h"        // <= Biblioteca sAPI
-
-// FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
-int main( void )
-{
-   // ---------- CONFIGURACIONES ------------------------------
-
-   // Inicializar y configurar la plataforma
-   boardConfig();
-
-   // Crear varias variables del tipo booleano
-   bool_t buttonValue = OFF;
-   bool_t ledValue    = OFF;
-   // Crear variable del tipo tick_t para contar tiempo
-   tick_t timeCount   = 0;
-
-   // ---------- REPETIR POR SIEMPRE --------------------------
-   while( TRUE ) {
-
-      /* Retardo bloqueante durante 100ms */
-      
-      delay( 100 );
-      
-      /* Si pasaron 10 segundos comienza a funcionar el programa que copia las
-         acciones en BOTON al LED. Mientras espera titila el LED.  */
-      
-      timeCount++;      
-      
-      if( timeCount == 100 ){ // 100ms * 100 = 10s
-         
-         while( TRUE ) {
-            
-            /* Si se presiona CIAA_BOARD_BUTTON, enciende el CIAA_BOARD_LED */
-
-            // Leer pin conectado al boton.
-            buttonValue = gpioRead( CIAA_BOARD_BUTTON );
-            // Invertir el valor leido, pues lee un 0 (OFF) con boton
-            // presionado y 1 (ON) al liberarla.
-            buttonValue = !buttonValue;
-            // Escribir el valor leido en el LED correspondiente.
-            gpioWrite( CIAA_BOARD_LED, buttonValue );
-
-            /* Enviar a la salida estandar (UART_DEBUG) el estado del LED */
-            
-            // Leer el estado del pin conectado al led
-            ledValue = gpioRead( CIAA_BOARD_LED );
-            // Chequear si el valor leido es encedido
-            if( ledValue == ON ) {
-               // Si esta encendido mostrar por UART_USB "LED encendido."
-               printf( "LED encendido.\r\n" );
-            } else {
-               // Si esta apagado mostrar por UART_USB "LED apagado."
-               printf( "LED apagado.\r\n" );
+static void readline(uartMap_t uart, char *s, size_t z) {
+    bool_t eol = FALSE;
+    size_t n = 0;
+    *s = 0;
+    while(!eol) {
+        uint8_t c;
+        if (uartReadByte(uart, &c)) {
+            if (!iscntrl(c)) {
+                s[n] = (char) c;
+                uartWriteByte(uart, c);
+                if (n < z) {
+                    n++;
+                    s[n] = 0;
+                }
             }
-            delay( 250 );
-            
-         }
-      } else {
-         // Intercambiar el valor de CIAA_BOARD_LED
-         gpioToggle(CIAA_BOARD_LED);
-      }
-   }
+            switch (c) {
+            case '\r':
+            case '\n':
+                eol = TRUE;
+                break;
+            }
+        } else
+            sleepUntilNextInterrupt();
+    }
+}
 
-   // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
-   // directamenteno sobre un microcontroladore y no es llamado por ningun
-   // Sistema Operativo, como en el caso de un programa para PC.
-   return 0;
+static char *skipspaces(char *s, const char *end) {
+    while (*s && isspace((int)*s)) {
+        if (s >= end)
+            return NULL;
+        s++;
+    }
+    return s;
+}
+
+static char *nextspace(char *s, const char *end) {
+    while (*s && !isspace((int)*s)) {
+        if (s >= end)
+            return NULL;
+        s++;
+    }
+    return s;
+}
+
+static int parsearg(char *s, size_t max, const char **const argv, int maxargc) {
+    int argc = 0;
+    const char *end = s + max;
+    while (*s) {
+        s = skipspaces(s, end);
+        if (!s || !*s)
+            break;
+        argv[argc] = s;
+        argc++;
+        s = nextspace(s, end);
+        if (!s || !*s)
+            break;
+        *s++ = '\0';
+        if (argc >= maxargc)
+            break;
+    }
+    return argc;
+}
+
+static int exec(int argc, const char **argv) {
+    // TODO
+    printf("Try to exec %s with %d parameters:\n", argv[0], argc - 1);
+    for (int i=1; i<argc; i++)
+        printf("  %s\n", argv[i]);
+    return 0;
+}
+
+static void repl() {
+    static const char *argv[10];
+    printf(">> ");
+    fflush(stdout);
+    readline(UART_USB, buffer, sizeof(buffer));
+    int argc = parsearg(buffer, sizeof(buffer), argv, 10);
+    puts("");
+    if (argc)
+         exec(argc, argv);
+}
+
+int main( void ) {
+    boardConfig();
+    puts("Welcome!!!");
+    while( TRUE )
+        repl();
 }
